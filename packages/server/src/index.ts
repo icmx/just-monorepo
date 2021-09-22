8795922d517a7dd3e03af5ea2f936991c87545df
icmx <32983315+icmx@users.noreply.github.com>
2021-09-22 13:46:33 +0300

    add orders feature into server package

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 
+import { createOrder } from './orders';
 import { findAllVehicles } from './vehicles';
 
 const app = express();
@@ -9,6 +10,7 @@ const port = 3000;
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+app.post('/api/v1/orders', createOrder);
 app.get('/api/v1/vehicles', findAllVehicles);
 
 app.listen(port, () =>
---
+++ packages/server/src/orders/handlers.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+import { OrderDto } from '@just-monorepo/types';
+import { times, validate } from '@just-monorepo/utils';
+
+export const createOrder = async (request: Request, response: Response) => {
+  const order: OrderDto = request.body;
+
+  console.log(`Trying to create an order: ${JSON.stringify(order)}`);
+
+  await times.delay(2000);
+
+  if (validate.isValidOrder(order)) {
+    response.sendStatus(200);
+  } else {
+    response.sendStatus(400);
+  }
+};
