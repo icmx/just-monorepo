dcb11a32691e46b21bfa07bc4b51be27c04d4683
icmx <32983315+icmx@users.noreply.github.com>
2021-09-27 10:59:14 +0300

    add docs for times utilities of utils package

--- packages/utils/src/times/delay.ts
+++ packages/utils/src/times/delay.ts
@@ -1,2 +1,9 @@
+/**
+ * Creates a delay promise before some action.
+ * @param ms - Delay value in milliseconds.
+ * @returns
+ * @example
+ * delay(2000).then(() => console.log('Read this after 2 seconds!'));
+ */
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
--- packages/utils/src/times/repeat.ts
+++ packages/utils/src/times/repeat.ts
@@ -1,3 +1,9 @@
+/**
+ * Repeats some action specified number of times.
+ * @param callback - Action to perform, must be the function.
+ * @param times - Number of times to perform `callback`.
+ * @returns Array of values returned by `callback`, if it's not `void`
+ */
 export const repeat = <T = void>(callback: () => T, times: number) => {
   let result: T[] = [];
 
